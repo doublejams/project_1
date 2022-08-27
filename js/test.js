1cddb246a14a11a166096fcdd37fd56e1044dd2c
Meowscles <remez-jeam@gmail.com>
2022-08-27 20:44:53 +0300

    frst commit

---
+++ js/test.js
@@ -0,0 +1,12 @@
+const storeName = 'АТБ';
+const storeDescription = {
+    budget: 10000,
+    employees: [
+        'Толик', 'Еболик', 'Алкоголик'
+    ],
+    products: {
+        vodka: 100,
+        pivo: 25
+    },
+    open: true
+};
